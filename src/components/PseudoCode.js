6e7bb814bb6c6357c55229662d353aa5bfca6e2f
Aryamaan <pandeyaryamaan@gmail.com>
2021-04-06 03:32:11 +0530

    feat:PsedoCodeEditor

--- src/components/PseudoCode.js
+++ src/components/PseudoCode.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React from 'react'
+import AceEditor from "react-ace";
 
-export default function PseudoCode() {
-  return <div></div>;
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-github";
+
+const PseudoCode = () => {
+  return (
+    <div className="pl-2 h-70">
+    <button className="bg-red-600 justify-end">Hello CodeX</button>
+    <AceEditor
+    className="ace h-50 bg-gray-800 w-auto"
+    mode="python"
+    theme="github"
+    name="UNIQUE_ID_OF_DIV"
+    editorProps={{ $blockScrolling: true }}
+  />
+    </div>
+  );
 }
+
+export default PseudoCode
